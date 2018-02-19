923a9f8a01de9ee9df8318eeb023b378ed128965
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-02-19 13:05:09 +0100

    Add project files.

---
+++ klubovna/Controllers/PresenceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace klubovna.Controllers
+{
+    [Route("api/[controller]")]
+    public class PresenceController : Controller
+    {
+        private bool _presence = false;
+
+        // GET api/presence
+        [HttpGet]
+        public bool Get()
+        {
+            return _presence;
+        }
+
+        // POST api/presence
+        [HttpPost]
+        public void Post([FromBody]bool value)
+        {
+            _presence = value;
+        }
+    }
+}
