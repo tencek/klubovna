a9f047a81014008f403fc5457cdc013bfa35d9b0
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-03-03 00:43:34 +0100

    + adding swagger

--- klubovna/Startup.cs
+++ klubovna/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace klubovna
 {
@@ -24,6 +25,11 @@ namespace klubovna
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Klubovna API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,13 @@ namespace klubovna
             }
 
             app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Klubova API V1");
+            });
         }
     }
 }
